f199e25f92a32d17e432f37ff4b2c81aa3700a05
Yuliia Kruhliak <Yuliia_Kruhliak@epam.com>
2022-10-17 03:04:06 +0300

    Deleted unused files

--- OnlineGameStore.Application/Helpers/IImageControl.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace OnlineGameStore.Application.Helpers
-{
-    public interface IImageControl
-    {
-        Task<string> UploadImageAsync(IFormFile image, string folder);
-    }
-}
