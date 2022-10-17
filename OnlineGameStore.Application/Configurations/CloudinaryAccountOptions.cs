3fc353bd63e253cc4c3813d2b8b776357410c5e2
Yuliia Kruhliak <Yuliia_Kruhliak@epam.com>
2022-10-17 03:06:43 +0300

    Deleted unused fields

--- OnlineGameStore.Application/Configurations/CloudinaryAccountOptions.cs
+++ OnlineGameStore.Application/Configurations/CloudinaryAccountOptions.cs
@@ -5,13 +5,5 @@ namespace OnlineGameStore.Api.Configurations
     public class CloudinaryAccountOptions : Account
     {
         public const string SectionName = "AccountSettings";
-/*
-        public string CloudName { get; set; } = string.Empty;
-
-        public string ApiKey { get; set; } = string.Empty;
-
-        public string ApiSecret { get; set; } = string.Empty;
-
-        public string ApiBaseAddress { get; set; } = string.Empty;*/
     }
 }
