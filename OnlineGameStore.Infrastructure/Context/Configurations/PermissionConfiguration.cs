493fc639c7ef54eadc1cb9f0b373e92feb2a0007
Yuliia Kruhliak <Yuliia_Kruhliak@epam.com>
2022-10-26 02:53:03 +0300

    Added tables for identity

---
+++ OnlineGameStore.Infrastructure/Context/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineGameStore.Infrastructure.Identity;
+
+namespace OnlineGameStore.Infrastructure.Context.Configurations;
+
+public class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+{
+    public void Configure(EntityTypeBuilder<Permission> builder)
+    {
+        builder.HasKey(permission => permission.Id);
+
+        builder.Property(permission => permission.Name);
+        builder.Property(permission => permission.Description);
+    }
+}
